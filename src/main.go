70e52428061b9036b87b376bb83f6bc55e6aedd1
holmes zhang <holmeszyx@gmail.com>
2014-10-02 21:57:03 +0800

    加入异或加密流

--- src/main.go
+++ src/main.go
@@ -10,15 +10,29 @@ import (
 func main(){
 	log.SetOutput(os.Stdout)
 	args := os.Args
-	if len(args) < 2{
-		log.Println("file args no found. xcat file ")
+	argsLen := len(args)
+	if argsLen < 2{
+		log.Fatalln("file args no found. xcat file ")
 		os.Exit(1)
 	}
 
-	filePath := args[1]
+	arg := args[1]
+	var filePath string
+	if argsLen >= 3 && arg == "-x"{
+		filePath = args[2]
+		writeX(filePath)
+	}else{
+		filePath = args[1]
+		readX(filePath)
+	}
+
+}
+
+// 读异或流
+func readX(filePath string){
 	file, e := os.Open(filePath)
 	if e != nil{
-
+		log.Fatalln("file open error")
 	}
 	defer file.Close()
 	xReader := xcat.NewXReader(file)
@@ -30,5 +44,24 @@ func main(){
 		}
 
 		fmt.Println(string(buff[:n]))
+	}
+}
+
+func writeX(filePath string){
+	file, e := os.Open(filePath)
+	if e != nil{
+		log.Fatalln("file open error")
+	}
+	defer file.Close()
+
+	xWriter := xcat.NewXWriter(os.Stdout)
+
+	buff := make([]byte, 4096)
+	for{
+		n, err := file.Read(buff)
+		if err != nil{
+			break;
+		}
+		xWriter.Write(buff[:n])
 	}
 }
--- src/xcat/xcat.go
+++ src/xcat/xcat.go
@@ -2,7 +2,7 @@ package xcat
 
 import (
 	"io"
-//	"encoding/binary"
+	"errors"
 )
 
 const (
@@ -10,16 +10,19 @@ const (
 	posOffset = 2
 )
 
+// 异或读流
 type XReader struct {
 	in io.Reader
 	pos uint32
 }
 
+// 新建一个异或读流
 func NewXReader(in io.Reader) (x *XReader){
 	x = &XReader{in, posOffset}
 	return
 }
 
+// 读
 func (x *XReader) Read(p []byte) (n int, e error){
 	n, e = x.in.Read(p)
 	if e != nil{
@@ -29,9 +32,6 @@ func (x *XReader) Read(p []byte) (n int, e error){
 	for i := 0; i < n; i ++{
 
 		po := (x.pos * x.pos * x.pos) >> 1
-//		pe := make([]byte, 4)
-//		binary.LittleEndian.PutUint32(pe, po)
-//		po = binary.BigEndian.Uint32(pe)
 		dk := byte(po)
 
 		f := dk ^ KEY ^ p[i]
@@ -41,3 +41,39 @@ func (x *XReader) Read(p []byte) (n int, e error){
 
 	return
 }
+
+// 异或写流
+type XWriter struct {
+	out io.Writer
+	pos uint32
+}
+
+// 新建一个异或写流
+func NewXWriter(out io.Writer) (x *XWriter){
+	x = &XWriter{out, posOffset}
+	return
+}
+
+// 写
+func (x *XWriter)Write(p []byte) (n int, err error){
+	pLen := len(p)
+	if pLen <= 0{
+		return 0, errors.New("write a empty buff")
+	}
+
+	for i := 0; i < pLen; i ++{
+		data := p[i]
+
+		po := (x.pos * x.pos * x.pos) >> 1
+		dk := byte(po)
+		f := dk ^ KEY ^ data
+
+		p[i] = f
+		x.pos ++
+	}
+
+	n, err = x.out.Write(p)
+	return
+}
+
+
